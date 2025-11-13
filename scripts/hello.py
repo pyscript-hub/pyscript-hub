ca538b69c7892413d9475fbdff15bf3e4bb648ad
pyscript-hub <pyscript-hub@proton.me>
2025-11-13 11:56:56 +0100

    Create hello.py

---
+++ scripts/hello.py
@@ -0,0 +1,8 @@
+SCRIPT_INFO = {
+    "version": "1.0",
+    "description": "Say hello."
+}
+
+def main(name="World"):
+    """Print a Hello message with optional name argument."""
+    print(f"👋 Hello, {name}!")
