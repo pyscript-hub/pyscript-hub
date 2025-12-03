137e344cab4a394e5fa4b71cbfe26902edfc65f1
pyscript-hub <pyscript-hub@proton.me>
2025-12-03 08:42:22 +0100

    Update hello.py

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,8 +1,3 @@
-SCRIPT_INFO = {
-    "version": "1.0",
-    "description": "Say hello."
-}
-
 def main(name="World"):
     """Print a Hello message with optional name argument."""
     print(f"👋 Hello, {name}!")
